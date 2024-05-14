c382501f4065fe5668288f3a2f79b49db8c7b8c0
HaxelKmil <hazeltampan14@gmail.com>
2024-05-14 17:02:05 +0700

    coba git pull

--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Avatar, Card, Col, Row, Button, Modal, Input, Upload, message} from "antd";
-import { PlusOutlined, UploadOutlined } from '@ant-design/icons';
+import { Avatar, Card, Col, Row, Button, Modal, Input, Upload, message, Dropdown, Menu, Divider} from "antd";
+import { PlusOutlined, UploadOutlined, UserOutlined, DownOutlined, ArrowLeftOutlined } from '@ant-design/icons';
 import Chart from 'chart.js/auto';
 import type { UploadFile } from 'antd';
 
 
-const { Meta } = Card;
+const { Item } = Menu;
 
 const Page = () => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -18,7 +18,22 @@ const Page = () => {
   const [sandi, setSandi] = useState('');
   const [konfirmasiSandi, setKonfirmasiSandi] = useState('');
   const [fileList, setFileList] = useState<UploadFile[]>([]);
-    
+
+
+  const menu = (
+    <Menu>
+      <Item key="1">
+        <a target="_blank" rel="noopener noreferrer" href="https://www.antgroup.com">
+          <UserOutlined style={{ marginRight: '10px' }}/>Profil
+        </a>
+      </Item>
+      <Item key="2">
+        <a style={{ color: 'red'}} target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+        <ArrowLeftOutlined style={{ color: 'red', marginRight: '10px' }}/>Keluar
+        </a>
+      </Item>
+    </Menu>
+  );
     const handleChange = (info: any) => {
       let fileList = [...info.fileList];
     
@@ -188,87 +203,97 @@ const handleSave = () => {
                 <p style={{ paddingBottom: '20px'}}>Halo, Elisabet. Selamat Datang di Inventaris!</p>
                 <Row gutter={[40, 40]}>
                     <Col>
-                        <Card className="shadow-card" style={{ width: '300px', height: '150px'}}>
-                            <Meta
-                            avatar={<img src="ikon.png"style={{ width: '50px', }} />}
-                            style={{ padding: '30px 30px 10px'}}
-                            title="20"
-                            description="Barang"
-                            />  
-                        </Card>
+                    <Card className="shadow-card" style={{ width: '300px', height: '150px', display: 'flex', alignItems: 'center' }}>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                          <img src="ikon.png" style={{ width: '100px', marginRight: '10px' }} />
+                          <div>
+                            <div style={{ fontSize: '20px', fontWeight: 'bold' }}>20</div>
+                            <div>Barang</div>
+                          </div>
+                        </div>
+                      </Card>
                     </Col>
-                    <Col>
-                        <Card className="shadow-card" style={{ width: '300px', height: '150px'}}>
-                            <Meta
-                            avatar={<img src="ikon.png" style={{ width: '50px'}} />}
-                            style={{ padding: '30px 30px 10px'}}
-                            title="3"
-                            description="Peminjam"
-                            />
-                        </Card>
+                    <Col style={{ marginLeft: '95px'}}>
+                      <Card className="shadow-card" style={{ width: '300px', height: '150px', display: 'flex', alignItems: 'center' }}>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                          <img src="ikon.png" style={{ width: '100px', marginRight: '10px' }} />
+                          <div>
+                            <div style={{ fontSize: '20px', fontWeight: 'bold' }}>3</div>
+                            <div>Peminjam</div>
+                          </div>
+                        </div>
+                      </Card>
                     </Col>
-                    <Col>
-                        <Card className="shadow-card" style={{ width: '300px', height: '150px'}}>
-                            <Meta
-                            avatar={<img src="ikon.png" style={{ width: '50px'}} />}
-                            style={{ padding: '30px 30px 10px'}}
-                            title="5"
-                            description="Aktif"
-                            />
-                        </Card>
+                    <Col style={{ marginLeft: '95px'}}>
+                    <Card className="shadow-card" style={{ width: '300px', height: '150px', display: 'flex', alignItems: 'center' }}>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                          <img src="ikon.png" style={{ width: '100px', marginRight: '10px' }} />
+                          <div>
+                            <div style={{ fontSize: '20px', fontWeight: 'bold' }}>5</div>
+                            <div>Aktif</div>
+                          </div>
+                        </div>
+                      </Card>
                     </Col>
-                    <Button type="primary" onClick={handleButtonClick} icon={<PlusOutlined />} style={{ marginTop: '110px', marginLeft: '210px'}}>Akun Petugas</Button>
+                    <Button type="primary" onClick={handleButtonClick} icon={<PlusOutlined />} style={{ marginTop: '90px', marginLeft: '110px'}}>Akun Petugas</Button>
                 </Row>
                 <Modal
-                title="Buat Akun Petugas"
+                title={<div style={{ fontSize: '20px',  fontWeight: 'bold'}}>Buat Akun Petugas</div>}
                 style={{ textAlign: 'center'}}
                 width={900}
                 visible={modalVisible}
                 onCancel={handleModalCancel}
                 footer={[
-                    <Button key="cancel" onClick={handleModalCancel} danger>Batal</Button>,
-                    <Button key="save" type="primary" onClick={handleSave}>Simpan</Button>
+                    <Button key="cancel" onClick={handleModalCancel}>Batal</Button>,
+                    <Button style={{ marginRight: '27px'}} key="save" type="primary" onClick={handleSave}>Simpan</Button>
                 ]}
+                maskStyle={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent black background
+                }}
                 >
-                 <div style={{ marginTop: '50px' }}> {/* Menambahkan margin atas */}
+                 <div style={{ marginTop: '70px', marginRight: '70px' }}> {/* Menambahkan margin atas */}
                  <Row gutter={[24, 24]}>
                     <Col span={12}>
                       <Row align="middle">
                         <Col span={8}>
                           <p>Nama</p>
                         </Col>
-                        <Col span={16}>
-                          <Input style={{ marginBottom: '12px' }} placeholder="Nama" value={nama} onChange={(e) => setNama(e.target.value)} />
+                        <Col>
+                          <Input style={{ marginBottom: '12px', width: '250px', height: '40px' }} placeholder="Nama" value={nama} onChange={(e) => setNama(e.target.value)} />
                         </Col>
                       </Row>
                       <Row align="middle">
                         <Col span={8}>
                           <p>NIP</p>
                         </Col>
-                        <Col span={16}>
-                          <Input style={{ marginBottom: '12px' }} type="string" placeholder="NIP" value={nip} onChange={(e) => setNIP(e.target.value)}  />
+                        <Col>
+                          <Input style={{ marginBottom: '12px', width: '250px', height: '40px' }} type="string" placeholder="NIP" value={nip} onChange={(e) => setNIP(e.target.value)}  />
                         </Col>
                       </Row>
                       <Row align="middle">
                         <Col span={8}>
                           <p>Telp</p>
                         </Col>
-                        <Col span={16}>
-                          <Input style={{ marginBottom: '12px' }} type="string" placeholder="Telp" value={telp} onChange={(e) => setTelp(e.target.value)} maxLength={12} />
+                        <Col>
+                          <Input style={{ marginBottom: '12px', width: '250px', height: '40px' }} type="string" placeholder="Telp" value={telp} onChange={(e) => setTelp(e.target.value)} maxLength={12} />
                         </Col>
                       </Row>
-                      <Row align="middle">
-                        <Col span={16}>
+                      <Row >
+                        <Col span={8}>
+                          <p>Unggah Foto</p>
+                        </Col>
+                        <Col>
                           <Upload
                             action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
                             listType="picture"
-                            fileList={fileList} 
+                            fileList={fileList}
                             onChange={handleChange}
                           >
-                            <Button icon={<UploadOutlined />}>Unggah</Button>
+                            <Button  icon={<UploadOutlined />}>Unggah</Button>
                           </Upload>
-                          <br />
-                          <br />
                         </Col>
                       </Row>
                     </Col>
@@ -277,24 +302,24 @@ const handleSave = () => {
                         <Col span={8}>
                           <p>Nama Pengguna</p>
                         </Col>
-                        <Col span={16}>
-                          <Input style={{ marginBottom: '12px' }} placeholder="Nama Pengguna" value={namaPengguna} onChange={(e) => setNamaPengguna(e.target.value)}/>
+                        <Col span={8}>
+                          <Input style={{ marginBottom: '12px', width: '300px', height: '40px' }} placeholder="Nama Pengguna" value={namaPengguna} onChange={(e) => setNamaPengguna(e.target.value)}/>
                         </Col>
                       </Row>
                       <Row align="middle">
                         <Col span={8}>
                           <p>Sandi</p>
                         </Col>
-                        <Col span={16}>
-                          <Input.Password style={{ marginBottom: '12px' }} placeholder="Sandi" value={sandi} onChange={(e) => setSandi(e.target.value)} />
+                        <Col span={8}>
+                          <Input.Password style={{ marginBottom: '12px', width: '300px', height: '40px' }} placeholder="Sandi" value={sandi} onChange={(e) => setSandi(e.target.value)} />
                         </Col>
                       </Row>
                       <Row align="middle">
                         <Col span={8}>
                           <p>Konfirmasi Sandi</p>
                         </Col>
-                        <Col span={16}>
-                          <Input.Password style={{ marginBottom: '12px' }} placeholder="Konfirmasi Sandi" value={konfirmasiSandi} onChange={(e) => setKonfirmasiSandi(e.target.value)} />
+                        <Col span={8}>
+                          <Input.Password style={{ marginBottom: '12px', width: '300px', height: '40px' }} placeholder="Konfirmasi Sandi" value={konfirmasiSandi} onChange={(e) => setKonfirmasiSandi(e.target.value)} />
                         </Col>
                       </Row>
                     </Col>
@@ -313,29 +338,47 @@ const handleSave = () => {
                     </Card>
                 </Col>
                 <Col>
-                    <Card className="shadow-card" style={{ width: '300px', height: '450px', padding: '50px 40px ', marginRight: '50px'}}>
-                      <Meta
-                        avatar={<img src="ikon.png" style={{ width: '90px' }} />}
-                        title="20"
-                        style={{ marginBottom: '20px'}}
-                        description="Barang"
-                      />
-                      <Meta
-                        avatar={<img src="ikon.png" style={{ width: '90px' }} />}
-                        title="20"
-                        style={{ marginBottom: '20px'}}
-                        description="Barang"
-                      />
-                      <Meta
-                        avatar={<img src="ikon.png" style={{ width: '90px' }} />}
-                        title="20"
-                        description="Barang"
-                      />
+                    <Card className="shadow-card" style={{ width: '300px', height: '450px', padding: '20px 20px 0'}}>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                          <img src="ikon.png" style={{ width: '100px', marginRight: '10px' }} />
+                            <div>
+                              <div style={{ fontSize: '20px', fontWeight: 'bold'}}>10</div>
+                              <div style={{ color: 'grey'}} >Barang Masuk</div>
+                            </div>
+                        </div>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                          <img src="ikon.png" style={{ width: '100px', marginRight: '10px' }} />
+                          <div>
+                            <div style={{ fontSize: '20px', fontWeight: 'bold' }}>4</div>
+                            <div style={{ color: 'grey'}}>Barang Keluar</div>
+                          </div>
+                        </div>
+                        <div style={{ display: 'flex', alignItems: 'center' }}>
+                          <img src="ikon.png" style={{ width: '100px', marginRight: '10px' }} />
+                          <div>
+                            <div style={{ fontSize: '20px', fontWeight: 'bold' }}>2</div>
+                            <div style={{ color: 'grey'}}>Barang Rusak</div>
+                          </div>
+                        </div>
                     </Card>
                   </Col>
             </Row>
-        </div>
-
+            <div style={{ position: 'absolute', top: '20px', right: '100px', display: 'flex', alignItems: 'center'}}>
+              <Dropdown overlay={menu} placement="bottomCenter">
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                  <Button style={{ width: '170px', height: '50px', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <img src="ikon.png" style={{ width: '70px', marginRight: '5px', marginLeft: '-10px'}} />
+                      <div>
+                          <div style={{ fontSize: '10px', color: 'black', marginRight: '20px'}}>Halo, Elisabet</div>
+                        <div  style={{ fontSize: '10px', color: 'grey ', marginRight: '47px'}}>Admin</div>
+                      </div>
+                    </div>
+                  </Button>
+                </div>
+              </Dropdown>
+            </div>
+        </div>  
         </div>
     );
 };

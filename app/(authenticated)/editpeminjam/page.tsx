55af95d1bb861c5397078fbdcee597285353b9bf
HaxelKmil <148941512+HaxelKmil@users.noreply.github.com>
2024-05-27 17:10:14 +0700

    push

--- app/(authenticated)/editpeminjam/page.tsx
+++ app/(authenticated)/editpeminjam/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+"use client";
 
-export default function page() {
+import { Button, Card } from 'antd';
+import React from 'react';
+import {  ArrowLeftOutlined } from '@ant-design/icons';
+
+const editpeminjam = () => {
   return (
-    <div>
-      <h1>Edit Peminjam</h1>
+    <div style={{ marginLeft: '50px'}}>
+      <title>Edit Peminjam</title>
+      <h1 style={{ fontSize: '25px', fontWeight: 'bold', marginTop: '70px'}}>Edit Peminjam</h1>
+      <Card style={{ marginTop: '30px',  boxShadow: '0px 7px 10px rgba(0, 0, 0, 0.1)', width: '50%'}}>
+        
+      </Card>
+      <div style={{ display: 'flex', justifyContent: 'flex-end', width: '50%'}}>
+        <Button style={{ marginTop: '20px', backgroundColor: '#582DD2', color: 'white' }}>
+          <a href='http://localhost:3001/peminjam'><ArrowLeftOutlined style={{ marginRight: '10px' }} />Kembali</a>
+        </Button>
+      </div>  
     </div>
   );
 }
+
+export default editpeminjam;
+

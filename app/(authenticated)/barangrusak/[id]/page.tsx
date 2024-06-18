9250a7a32388030155001e3fd2e9537be65216e4
wanta9 <ridwan0299999@gmail.com>
2024-06-18 16:45:05 +0700

    halodek

--- app/(authenticated)/barangrusak/[id]/page.tsx
+++ app/(authenticated)/barangrusak/[id]/page.tsx
@@ -19,6 +19,7 @@ const barangRusak = ({ params }: { params: { id: string } }) => {
   return (
     <div style={{ marginLeft: '50px', fontFamily }}>
       <title>Barang Rusak</title>
+<<<<<<< Updated upstream
       <h1 style={{ fontSize: '25px', fontWeight: 'bold', marginTop: '30px' }}>
         Detail Barang Rusak
       </h1>
@@ -106,6 +107,55 @@ const barangRusak = ({ params }: { params: { id: string } }) => {
           </Col>
         </Row>
       </Card>
+=======
+      <h1 style={{ fontSize: '25px', fontWeight: 'bold', marginTop: '30px' }}>Detail Barang Rusak</h1>
+      <Card style={{ width: '80%', marginTop: '40px', boxShadow: '0px 7px 10px rgba(0, 0, 0, 0.1)' }}>
+      <Row align="middle" justify="center">
+        <Col span={12} style={{ display: 'flex', justifyContent: 'center' }}>
+          <div style={{ width: '100%', height: '400px', backgroundColor: '#D9D9D9', borderRadius: '20px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <img src="kk.png" alt="gambar" style={{ width: '70%', borderRadius: '20px' }} />
+          </div>
+        </Col>
+        <Col span={12} style={{ paddingLeft: '40px', marginBottom: '30px'}}>
+          <Row style={{ marginBottom: '30px', fontSize: '16px'}}>
+            <Col span={9} style={{ fontWeight }}>Kode Barang</Col>
+            <Col span={3}>:</Col>
+            <Col span={5}>{barangRusakById?.data?.kode}</Col>
+          </Row>
+          <Row style={{ marginBottom: '30px'}}>
+            <Col span={9} style={{ fontWeight }}>Nama Barang</Col>
+            <Col span={3}>:</Col>
+            <Col span={5}>{barangRusakById?.data?.ruanganBarang?.barang?.nama}</Col>
+          </Row>
+          <Row style={{ marginBottom: '30px'}}>
+            <Col span={9} style={{ fontWeight }}>Jumlah Barang</Col>
+            <Col span={3}>:</Col>
+            <Col span={5}>{barangRusakById?.data?.jumlah}</Col>
+          </Row>
+          <Row style={{ marginBottom: '30px'}}>
+            <Col span={9} style={{ fontWeight }}>Tanggal Rusak</Col>
+            <Col span={3}>:</Col>
+            <Col span={5}>{formattedDate}</Col>
+          </Row>
+          <Row style={{ marginBottom: '30px'}}>
+            <Col span={9} style={{ fontWeight }}>Status</Col>
+            <Col span={3}>:</Col>
+            <Col span={5}><Button style={{ color: '#5BFF00', backgroundColor: 'rgba(162, 225, 129, 0.3)', borderColor: '#A2E181'}}>{barangRusakById?.data?.status}</Button></Col>
+          </Row>
+          <Row style={{ marginBottom: '30px'}}>
+            <Col span={9} style={{ fontWeight }}>Ruangan</Col>
+            <Col span={3}>:</Col>
+            <Col span={5}>RPL</Col>
+          </Row>
+          <Row style={{ marginBottom: '20px'}}>
+            <Col span={9} style={{ fontWeight }}>Keterangan</Col>
+            <Col span={3}>:</Col>
+            <Col span={12} style={{  display: 'flex', alignItems: 'center'}}>{barangRusakById?.data?.keterangan}</Col>
+          </Row>
+        </Col>
+      </Row>
+    </Card>
+>>>>>>> Stashed changes
       <div style={{ display: 'flex', justifyContent: 'flex-end', width: '80%' }}>
         <Button
           style={{

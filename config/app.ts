bace712bb2e992b70f687c4b2c7e23a1f0eaccd1
wanta9 <ridwan0299999@gmail.com>
2024-07-29 11:51:55 +0700

    halodek

--- config/app.ts
+++ config/app.ts
@@ -2,9 +2,5 @@ export const config = {
   baseUrl:
     (typeof window !== 'undefined' ? (window as any).serverEnv?.DYNAMIC_ENV_BASE_URL : '') ||
     process.env.NEXT_PUBLIC_BASE_URL ||
-<<<<<<< HEAD
-    'http://localhost:3222',
-=======
     'http://172.17.2.91:3222',
->>>>>>> bbe908c97f7acc8d1549c0862e429af18339d770
 };
